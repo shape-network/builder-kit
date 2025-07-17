5732a3175979b1523879adedea40fd06c4222faa
William H <william.hermozo@protonmail.com>
2025-07-17 13:02:13 +0200

    Revert "add nft lookup"
    This reverts commit abad9636e55f833f08b18e4ba93f6bccba7bebc0.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { ShapeNftViewer } from '@/components/shape-nft-viewer';
+import { McpGreetDemo } from '@/components/mcp-greet';
 
 export default function Home() {
   return (
-    <div className="flex min-h-[calc(100vh-200px)] flex-col justify-start">
-      <ShapeNftViewer />
+    <div className="flex min-h-[calc(100vh-200px)] flex-col items-center justify-center space-y-8">
+      <div className="w-full max-w-4xl">
+        <McpGreetDemo />
+      </div>
     </div>
   );
 }
